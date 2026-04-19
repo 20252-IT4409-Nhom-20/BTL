c61fde7c8afb4e5626c41d4f970356ab27a5d546
Minh <minh1604.work@gmail.com>
2026-04-20 03:23:54 +0700

    Login and Registry pages

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { MainLayout } from '@/components/MainLayout';
 import StoriesPage from '@/pages/StoriesPage';
+import LoginPage from '@/pages/LoginPage';
+import RegisterPage from '@/pages/RegisterPage';
+import ItemPage from '@/pages/ItemPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
+          <Route path="/item/:id" element={<ItemPage />} />
           <Route path="/:type" element={<StoriesPage />} />
           <Route path="/" element={<Navigate to="/news" replace />} />
         </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
