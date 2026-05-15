c712010dc5d20ec153375da07d8725777b9d804a
scev <minh.nn210582@sis.hust.edu.vn>
2026-05-15 17:52:38 +0700

    feat(fe-batch-fetch): added some options to pagination

--- fe/src/pages/StoriesPage.tsx
+++ fe/src/pages/StoriesPage.tsx
@@ -68,6 +68,8 @@ export default function StoriesPage() {
           value={limit}
           onChange={(e) => handleLimitChange(Number(e.target.value))}
         >
+          <option value={10}>10</option>
+          <option value={20}>20</option>
           <option value={30}>30</option>
           <option value={40}>40</option>
           <option value={50}>50</option>
